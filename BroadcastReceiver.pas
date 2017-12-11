89b757d41de079ceee64e17f7c5745f16aaff647
maxstepanovski <stepanov_maks@mail.ru>
2017-12-11 16:42:01 +0300

    broadcast receiver

---
+++ BroadcastReceiver.pas
@@ -0,0 +1,32 @@
+unit BroadcastReceiver;
+
+interface
+
+uses
+  Androidapi.JNIBridge,
+  Androidapi.JNI.Embarcadero,
+  Androidapi.JNI.GraphicsContentViewText,
+  Androidapi.Helpers,
+  Androidapi.JNI.JavaTypes,
+  FMX.Types;
+
+type
+  TMyReceiver = class(TJavaLocal, JFMXBroadcastReceiverListener)
+  public
+    constructor Create;
+    procedure onReceive(context: JContext; intent: JIntent); cdecl;
+  end;
+
+implementation
+
+constructor TMyReceiver.Create;
+begin
+  inherited;
+end;
+
+procedure TMyReceiver.onReceive(context: JContext; intent: JIntent);
+begin
+  Log.d('Broadcast Received = ' + JStringToString(intent.getAction));
+end;
+
+end.
--- Unit1.pas
+++ Unit1.pas
@@ -4,12 +4,18 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, Androidapi.JNIBridge,
+  Androidapi.JNI.Embarcadero, Androidapi.Helpers, BroadcastReceiver,
+  Androidapi.JNI.JavaTypes, Androidapi.JNI.GraphicsContentViewText;
 
 type
   TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
     { Private declarations }
+    FMyListener: TMyReceiver;
+    FBroadcastReceiver: JFMXBroadcastReceiver;
   public
     { Public declarations }
   end;
@@ -20,5 +26,21 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  Filter: JIntentFilter;
+begin
+  FMyListener := TMyReceiver.Create;
+  FBroadcastReceiver := TJFMXBroadcastReceiver.JavaClass.init(FMyListener);
+  Filter := TJIntentFilter.JavaClass.init;
+  Filter.addAction(TJIntent.JavaClass.ACTION_VIEW);
+  TAndroidHelper.context.getApplicationContext.registerReceiver(FBroadcastReceiver, Filter);
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  TAndroidHelper.context.getApplicationContext.unregisterReceiver(FBroadcastReceiver);
+end;
 
 end.
